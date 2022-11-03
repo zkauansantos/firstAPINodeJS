dc80b1b662ab83f7f464e5554fbc5eae6113640a
zkauansantos <kauansantosdepontes@gmail.com>
2022-11-03 13:41:54 -0300

    working with query params with method parse depecrated

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 const http = require('http');
+const { parse } = require('path');
 const url = require('url')
 
-const routes = require('./routes.')
+const routes = require('./routes')
 
 const server = http.createServer((request, response) => {
-    console.log(`Request method: ${request.method} | Endpoint: ${request.url} `);
+    const parsedUrl = url.parse(request.url, true)
+
+    console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname} `);
 
     const route = routes.find((routeObj) =>  (
-        routeObj.endpoint === request.url && routeObj.method === request.method
+        routeObj.endpoint === parsedUrl.pathname && routeObj.method === request.method
     ))
 
     if(route){
+        request.query = parsedUrl.query
         route.handler(request, response)
     }
     else{
